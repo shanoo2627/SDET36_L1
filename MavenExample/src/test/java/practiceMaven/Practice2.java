d6ce70f39ba33a24a6123c86fcfdbd7f11c0fac9
shanoo <shanoo@DESKTOP-UVLAPAT>
2022-07-19 12:28:56 +0530

    3rd Commit

---
+++ MavenExample/src/test/java/practiceMaven/Practice2.java
@@ -0,0 +1,23 @@
+package practiceMaven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice2 {
+	
+	@Test
+	
+	public void SignIn() {
+		Reporter.log("Hi");
+		Reporter.log("Hello");
+		Reporter.log("AddToCart");
+			
+	}
+	@Test
+	public void SignOut() {
+		Reporter.log("null");
+		
+		
+	}
+}
+
