c1eab2bb65eabbb91540e0073070312072b332f8
shanoo <shanoo@DESKTOP-UVLAPAT>
2022-07-19 11:43:55 +0530

    1st Commit

---
+++ MavenExample/src/test/java/practiceMaven/Practice1.java
@@ -0,0 +1,22 @@
+package practiceMaven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	
+	@Test
+	
+	public void SignIn() {
+		Reporter.log("Signin");
+		
+		
+	}
+	@Test
+	public void SignOut() {
+		Reporter.log("SignOut");
+		
+		
+	}
+}
+
