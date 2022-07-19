b13fcb5a6fe163b3e8b273040ba6ac73df31eb7f
shanoo <shanoo@DESKTOP-UVLAPAT>
2022-07-19 15:32:22 +0530

    commit13

--- MavenExample/src/test/java/practiceMaven/Practice1.java
+++ MavenExample/src/test/java/practiceMaven/Practice1.java
@@ -4,27 +4,22 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Practice1 {
-	
+	//Good Morning
 	@Test
-	
 	public void SignIn() {
 		Reporter.log("Signin");
 		Reporter.log("HomePage");
 		Reporter.log("AddToCart");		
 	}
+	
+	//Good evening
 	@Test
 	public void SignOut() {
 		Reporter.log("SignOut");
 		Reporter.log("Signin");
-		Reporter.log("HomePage");
-		Reporter.log("AddToCart");	
+		//Reporter.log("HomePage");
+		//Reporter.log("AddToCart");	
 	}
-	@Test
-	public void SignOut() {
-		Reporter.log("SignOut");
-		Reporter.log("Signin");
-		Reporter.log("HomePage");
-		Reporter.log("AddToCart");	
-	}
+	
 }
 
